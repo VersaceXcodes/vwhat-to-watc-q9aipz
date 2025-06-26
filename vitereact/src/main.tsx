96ab5ac4fb4b0babb695a98263f8cdccf64edd24
VersaceXcodes <95313545+VersaceXcodes@users.noreply.github.com>
2025-06-26 20:02:42 +0100

    Initial deploy project vwhat-to-watc

---
+++ vitereact/src/main.tsx
@@ -0,0 +1,13 @@
+import { createRoot } from "react-dom/client";
+import AppWrapper from "./AppWrapper.tsx";
+import "./index.css";
+createRoot(document.getElementById("root")!).render(
+			<AppWrapper /> 
+);
+
+
+
+
+
+
+
